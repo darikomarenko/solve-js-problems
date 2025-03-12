b7a7766d152d570acee144bbe853dacf92f4cb11
Daria Komarenko <dkomarenko@spherical.pm>
2025-03-12 13:08:46 +0300

    Add logs and comments

--- unique-symbol.js
+++ unique-symbol.js
@@ -1,4 +1,10 @@
-// Вернуть true, если в строке все символы уникальные
+// Вернуть true, если в строке все символы уникальные, регистр должен учитываться
 function isUnique(string) {
-    return new Set(string) === string;
+    return new Set(string).size === string.length;
 };
+
+console.log(isUnique('abcdefg')); //true
+console.log(isUnique('adcdefg')); //false
+console.log(isUnique('')); //true
+console.log(isUnique('1234')); //true
+console.log(isUnique('ABCabc')); //true
